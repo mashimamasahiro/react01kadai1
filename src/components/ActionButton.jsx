9db31dd1e275874ee4b32832655a69fd2aa569d8
mashimamasahiro <masahiro.mashima@gmail.com>
2022-07-24 15:36:07 +0800

    main

---
+++ src/components/ActionButton.jsx
@@ -0,0 +1,12 @@
+// 🔽 propsを追加
+export const ActionButton = ({ text, action }) => {
+    return (
+      <>
+        {/* 🔽 `onClick` でクリック時に指定した関数を実行できる */}
+        <button type="button" onClick={action}>
+          {text}
+        </button>
+      </>
+    );
+  };
+  
